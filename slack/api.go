88f1616bd5595931bb367e167c59cfdca244317e
Stefano Torresi <stefano@torresi.io>
2017-10-20 17:16:49 +0200

    move api related stuff into dedicated package

---
+++ slack/api.go
@@ -0,0 +1,84 @@
+package slack
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+)
+
+const baseUrl = "https://api.slack.com/api/"
+
+type Api struct {
+	authToken string
+	client    *http.Client
+}
+
+type ApiError struct {
+	msg string
+}
+
+type Response struct {
+	Ok            bool   `json:"ok"`
+	Error         string `json:"error"`
+	RequestMethod string `json:"req_method"`
+}
+
+type ChannelMembers struct {
+	Response
+	Members []string `json:"members"`
+}
+
+type UserInfo struct {
+	Response
+	User struct {
+		Id      string `json:"id"`
+		Profile struct {
+			DisplayName        string `json:"display_name"`
+			RealName           string `json:"real_name"`
+			RealNameNormalized string `json:"real_name_normalized"`
+			Email              string `json:"email"`
+		} `json:"profile"`
+	} `json:"user"`
+}
+
+func New(authToken string, client *http.Client) *Api {
+	return &Api{authToken, client}
+}
+
+func (e *ApiError) Error() string {
+	return "API Error: " + e.msg
+}
+
+func isHttpError(r *http.Response) bool {
+	return r.StatusCode >= 400 && r.StatusCode < 600
+}
+
+func (api *Api) FetchChannelMembers(channelId string) (ChannelMembers, error) {
+	endpoint := "conversations.members"
+
+	response, err := api.client.PostForm(baseUrl+endpoint, url.Values{
+		"token":   {api.authToken},
+		"channel": {channelId},
+	})
+	if err != nil {
+		return ChannelMembers{}, &ApiError{err.Error()}
+	}
+	if isHttpError(response) {
+		return ChannelMembers{}, &ApiError{response.Status}
+	}
+
+	defer response.Body.Close()
+
+	return decodeChannelMembers(response)
+}
+
+func decodeChannelMembers(response *http.Response) (ChannelMembers, error) {
+	channelMembers := ChannelMembers{}
+	err := json.NewDecoder(response.Body).Decode(&channelMembers)
+
+	if err == nil && !channelMembers.Ok {
+		err = &ApiError{channelMembers.Error}
+	}
+
+	return channelMembers, err
+}
--- ssr.go
+++ ssr.go
@@ -1,68 +1,31 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/caarlos0/env"
 	"github.com/joho/godotenv"
-	"io/ioutil"
+	"github.com/stefanotorresi/ssr/slack"
 	"log"
 	"math/rand"
 	"net/http"
-	"net/url"
 	"time"
 )
-
-const slackApiBaseUrl = "https://api.slack.com/api/"
-const channelMembersEndpoint = slackApiBaseUrl + "conversations.members"
-const userInfoEndpoint = slackApiBaseUrl + "users.info"
 
 type config struct {
 	SlackAuthToken string `env:"SLACK_AUTH_TOKEN,required"`
 	ChannelId      string `env:"CHANNEL_ID,required"`
 }
 
-type SlackResponse struct {
-	Ok            bool   `json:"ok"`
-	Error         string `json:"error"`
-	RequestMethod string `json:"req_method"`
-}
-
-type ChannelMembers struct {
-	SlackResponse
-	Members []string `json:"members"`
-}
-
-type UserInfo struct {
-	SlackResponse
-	User struct {
-		Id      string `json:"id"`
-		Profile struct {
-			DisplayName        string `json:"display_name"`
-			RealName           string `json:"real_name"`
-			RealNameNormalized string `json:"real_name_normalized"`
-			Email              string `json:"email"`
-		} `json:"profile"`
-	} `json:"user"`
-}
-
-type ApiError struct {
-	msg string
-}
-
-func (e *ApiError) Error() string { return "API Error: " + e.msg }
-
 func init() {
 	rand.Seed(time.Now().Unix())
 
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file: " + err.Error())
+		log.Fatal("Error loading environment: " + err.Error())
 	}
 }
 
 func main() {
-
 	config := config{}
 	err := env.Parse(&config)
 	exitOnError(err)
@@ -71,77 +34,13 @@ func main() {
 		Timeout: time.Second * 10,
 	}
 
-	channelInfo, err := fetchChannelMembers(client, config)
+	api := slack.New(config.SlackAuthToken, client)
+
+	channelInfo, err := api.FetchChannelMembers(config.ChannelId)
 	exitOnError(err)
 
 	members := shuffle(channelInfo.Members)
-
-	userInfos := make([]UserInfo, len(members))
-	for i, memberId := range members {
-		results := make(chan UserInfo, len(members))
-		go func() {
-			userInfo, err := fetchUserInfo(client, memberId, config.SlackAuthToken)
-			exitOnError(err)
-			results <- userInfo
-		}()
-		userInfos[i] = <-results
-	}
-
-	for _, user := range userInfos {
-		fmt.Printf("%#v\n", user.User)
-	}
-}
-
-func fetchChannelMembers(client *http.Client, config config) (ChannelMembers, error) {
-	response, err := client.PostForm(channelMembersEndpoint, url.Values{
-		"token":   {config.SlackAuthToken},
-		"channel": {config.ChannelId},
-	})
-	if err != nil {
-		return ChannelMembers{}, err
-	}
-
-	defer response.Body.Close()
-
-	return decodeChannelMembers(response)
-}
-
-func fetchUserInfo(client *http.Client, memberId string, authToken string) (UserInfo, error) {
-	response, err := client.PostForm(userInfoEndpoint, url.Values{
-		"token": {authToken},
-		"user":  {memberId},
-	})
-	if err != nil {
-		return UserInfo{}, err
-	}
-
-	defer response.Body.Close()
-
-	return decodeUserInfo(response)
-}
-
-func decodeChannelMembers(response *http.Response) (ChannelMembers, error) {
-	channelMembers := ChannelMembers{}
-	err := json.NewDecoder(response.Body).Decode(&channelMembers)
-
-	if err == nil && !channelMembers.Ok {
-		err = &ApiError{channelMembers.Error}
-	}
-
-	return channelMembers, err
-}
-
-func decodeUserInfo(response *http.Response) (UserInfo, error) {
-	userInfo := UserInfo{}
-	body, err := ioutil.ReadAll(response.Body)
-
-	err = json.Unmarshal(body, &userInfo)
-
-	if err == nil && !(userInfo.Ok && userInfo.User.Id != "") {
-		err = &ApiError{fmt.Sprintf("%#v", userInfo)}
-	}
-
-	return userInfo, err
+	fmt.Printf("%v\n", members)
 }
 
 func exitOnError(err error) {
